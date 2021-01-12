d55f88b37ebffab187d4efd0731422f1acbb9e2d
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2021-01-12 08:50:08 +0300

    example_funkw.py: Example with "function" keyword instead of "lambda".
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

---
+++ example_funkw.py
@@ -0,0 +1,7 @@
+# Example for mod_funkw_naive.py/mod_funkw.py . To run:
+#
+#     python3 -m imphook -i mod_funkw_naive -m example_funkw
+#     python3 -m imphook -i mod_funkw -m example_funkw
+
+my_fun = function: print("imphook's functionality is cool!")
+my_fun()
