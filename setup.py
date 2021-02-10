377401fac62c7a8be010a7450645673f9123489c
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2021-02-10 11:14:40 +0300

    setup.py: Release 0.5.1.
    Signed-off-by: Paul Sokolovsky <pfalcon@users.sourceforge.net>

---
+++ setup.py
@@ -0,0 +1,13 @@
+import sys
+from setuptools import setup
+
+
+setup(name="imphook",
+      version="0.5.1",
+      description="""Simple and clear import hooks for Python - import anything as if it were a Python module""",
+      long_description=open('README.rst').read(),
+      url="https://github.com/pfalcon/python-imphook",
+      author="Paul Sokolovsky",
+      author_email="pfalcon@users.sourceforge.net",
+      license="MIT",
+      py_modules=["imphook"])
